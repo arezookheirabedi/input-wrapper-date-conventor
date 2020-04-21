b9afc26d0e033392fed1ad030a2574b6a881a7f5
arezookheirabadi <you@example.com>
2020-04-21 10:16:47 +0430

    add localization and AppRegex in App file

---
+++ src/config/localization/en.ts
@@ -0,0 +1,12 @@
+export const en = {
+    Email:"Email",
+    familyName:" familyName",
+    name:"name",
+
+    validation_msg: {
+        Just_enter_the_Email: "Just enter the email",
+        Just_enter_the_cell_number: "Just enter the mobile number",
+        missing_requiered_field: 'requiered field',
+ }
+
+}
---
+++ src/config/localization/fa.ts
@@ -0,0 +1,14 @@
+export const fa = {
+
+Email:"ایمیل",
+familyName:"نام خانوادگی",
+name:"نام",
+    validation_msg: {
+        Just_enter_the_Email : "فقط ایمیل را وارد کنید",
+        Just_enter_the_cell_number :"فقط شماره موبایل وارد کنید",
+        missing_requiered_field: 'موارد ضروری را پر کنید',
+    }
+  
+
+
+}
