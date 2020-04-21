3a3adaae8cf92ec55e8dadaa9fb0f61cfddc463c
arezookheirabadi <you@example.com>
2020-04-21 19:49:10 +0430

    add localization

--- src/config/localization/en.ts
+++ src/config/localization/en.ts
@@ -2,7 +2,7 @@ export const en = {
     Email:"Email",
     familyName:" familyName",
     name:"name",
-
+    phonenumber:"phonenumber",
     validation_msg: {
         Just_enter_the_Email: "Just enter the email",
         Just_enter_the_cell_number: "Just enter the mobile number",
