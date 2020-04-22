2e9d9da69c46fe5897dad68bb6f992d14e5c1510
arezookheirabadi <you@example.com>
2020-04-22 15:30:03 +0430

    add email pattern

--- src/app_input/inputWrapper.tsx
+++ src/app_input/inputWrapper.tsx
@@ -4,176 +4,104 @@ import { NpmInput } from "../npm_inpt/NpmInput";
 interface IProps {
 	value: string;
 	type?: string;
-	emailPattern?: RegExp;
-	numberPattern?:RegExp;
-	label: string;
+	label?: string;
 	isRequired?: boolean;
 	requiredMessageError?: string;
-	numberMassageError?: string;
-	emailMessageError?: string;
+	patternError?: string;
 	placeholder?: string;
-	onChange: (value: string) => void;
+	onChange: (value: string, isValid: boolean) => void;
+	Pattern?: RegExp;
 }
 interface IState {
-	
-	newMessage:string;
+	valid: boolean;
+	touch: boolean;
 }
+
 export class InputWrapper extends Component<IProps, IState> {
-	inputRef: any;
-	inputId = Math.random() + "";
 	constructor(props: IProps) {
 		super(props);
-		this.inputRef = React.createRef();
 		this.state = {
-		newMessage:""
+			valid: false,
+			touch: false,
 		};
 	}
-	starIsRequired = (isRequired: boolean) => {
-		if (this.props.isRequired === true) {
-			return <span className="text-danger">*</span>;
-		} else {
-			return;
-		}
-	};
-	setmessage(isvalid:boolean){
-		let newMessage:string = ""
-		if (!isvalid){
-			if (this.props.emailPattern && this.props.emailMessageError){
-				newMessage = this.props.emailMessageError
-			}
 
-			if (this.props.isRequired!&& this.props.requiredMessageError){
-				newMessage=this.props.requiredMessageError
-			}
-
-			if(this.props.numberPattern&&this.props.numberMassageError){
-				newMessage=this.props.numberMassageError
-			}
-			this.setState({
-				...this.state,
-				newMessage
-		 				
-			})
-		}
-	}
-
-
+	componentDidUpdate(prevProps: IProps) {}
+	inputId = Math.random() + "";
 
 	handlerInput(value: string) {
 		const isvalid = this.checkValidate(value);
-		this.setmessage(isvalid)
-		this.props.onChange(value);
-
+		this.props.onChange(value, isvalid);
 	}
-
-	// 	/**
-	// this function call on 2 condition
-	// 1.if field Required
-	// 2.user touch input and leave it without any action
-	// 3.inputError show text az an error
-	// */
-
-	// inputError = (isRequired: boolean, isvalid: boolean, isTuch: boolean) => {
-	// 	if (isRequired === true) {
-	// 		console.log(this.props);
-	// 		if (isvalid === false && isTuch === true) {
-	// 			return <span className="text-danger"> فیلد ضروری است</span>;
-	// 		}
-	// 		if (isvalid === true && isTuch === true) {
-	// 			return;
-	// 		}
-	// 		if (isvalid === false && isTuch === false) {
-	// 			return;
-	// 		}
-	// 	} else {
-	// 		return;
-	// 	}
-	// };
-
 	componentDidMount() {
-		// const el = document.getElementById(this.inputId);
-		// const e1 = el?.querySelector("inputId");
-		// (e1||{onblur: null}).onblur=()=>{this.setState({
-		// 	...this.state,
-		// 	isTuch: true,
-		// 	});
-		// 	console.log(this.state.isTuch)
-		// };
-		// 		 const el = document.getElementById(this.inputId)
-		// 		const e=el?.addEventListener('blur',  inputTuch =()=> {
-		// 			change to
-		// ()=>{
-		// 		 	this.setState({
-		// 					...this.state,
-		// 					isTuch: true,
-		// 					});
-		// 				}}
-		// 		// this.inputRef.current.onblur = () => {
-		// 		// 	debugger;
-		// 		// };
-		// 		// setTimeout(() => {
-		// 		// 	debugger;
-		// 		// 	const gholi = this.inputRef.current.props.onblur();
-		// 		// }, 2000);
+		//	debugger;
+		const thisEl = document.getElementById(this.inputId);
+		const htmlInput = thisEl?.querySelector("input");
+		htmlInput &&
+			htmlInput.addEventListener("blur", () => {
+				this.setState({ touch: true });
+			});
 	}
+	//checkValidate = (value: string): boolean => {
+	// 	// if (this.props.isRequired === true)
+	// 	// 	if (!!value)
+	// 	// 		return true;
+	// 	// 	else return false;
+	// 	// }
 
-	setemailPattern = (value: string) => {
-		if (value) {
-			if (this.props.emailPattern!.test(value)) {
+	// ValidateEmail =  => {
+	// 	if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+	// 	  return true
+	// 	}
+
+	checkValidate = (value: string): boolean => {
+		debugger;
+		if (this.props.Pattern) {
+			if (this.props.Pattern?.test(value)) {
 				return true;
-			} else {
-				return false;
 			}
-		}
-	};
-	setreguired(value: string) {
-		if (!value || !value.toString().trim().length) {
-			return false;
 		} else {
-			return true;
-		}
-	}
-	setNumberpattern(value: string) {
-		const length = value?value.toString().length : 0;
-		if (length > 0) {
-			if (this.props.numberPattern!.test(value)) {
-				return false;
-			} else {
+			if (this.props.isRequired === true && !!value) {
 				return true;
 			}
 		}
+		return false; // TODO: fal;something is wrong
+	};
+
+	renderErrors(): React.ReactNode {
+		const v = this.checkValidate(this.props.value);
+		if (v) return <></>;
+
+		if (this.props.isRequired === true) {
+			return (
+				<em className="text-danger">
+					{this.props.requiredMessageError}
+				</em>
+			);
+		}
+		if (!!this.props.Pattern) {
+			return <em className="text-danger">{this.props.patternError}</em>;
+		}
+
+		return <></>;
 	}
-	// checkValidate(value: string): boolean {
-	// 	return true
-	// }
-
-
-	checkValidate = ( value: string) => {
-
-			if (this.setemailPattern(value) === true && value !== null&&this.props.emailPattern!) {
-				return false
-			} 
-		
-		 if(this.setreguired(value)===true&& this.props.isRequired!){
-		return true
-			} 
-	
-			 if(this.setNumberpattern(value)===true&& this.props.numberPattern!){
-			return true
-				} 
-			return false;
-	}
-	
-	
-
 	render() {
 		return (
 			<div className="form-group" id={this.inputId}>
 				<div>
-					<label>
+					<label
+						className={
+							this.props.label === undefined ? "d-none" : ""
+						}
+					>
 						{this.props.label}
-						{this.starIsRequired(this.props.isRequired!)}
-						{/* aya be in shekl neveshtam doroste.ya dar props undefine ham tarif konam */}
+						<em
+							className={
+								this.props.isRequired === true ? "" : "d-none"
+							}
+						>
+							<span className="text-danger">*</span>
+						</em>
 					</label>
 				</div>
 
@@ -182,27 +110,10 @@ export class InputWrapper extends Component<IProps, IState> {
 					onChange={(e: string) => {
 						this.handlerInput(e);
 					}}
-					ref={this.inputRef}
 				/>
-				{/* <div>{this.checkValidate(this.props.value)}</div> */}
-				{/* <div>
-					{this.inputError(
-						this.props.isRequired!,
-						this.state.isValid!,
-						this.state.isTuch
-					)}
-				</div> */}
+
+				{this.state.touch ? this.renderErrors() : ""}
 			</div>
 		);
 	}
 }
-
-/**
-<input type="text" ref={element => this.textInput = element} />
-this.textInput.value
-  this.textInput = React.createRef();
-
-
-*/
-
-
